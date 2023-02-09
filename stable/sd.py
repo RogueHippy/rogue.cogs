08ae21cd0ae662f1629d2e133114c0d560db580a
RogueHippy <117488354+RogueHippy@users.noreply.github.com>
2023-02-08 23:04:41 -0700

    Update sd.py

--- stable/sd.py
+++ stable/sd.py
@@ -1,15 +1,20 @@
-import discord
 import requests
-from redbot.core import commands, Config
-from io import BytesIO
-from discord.ext import commands
+from redbot.core import commands
 
 class ImageGeneration(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @commands.command()
-    async def generate_image(self, ctx, text):
-        response = requests.get(f"https://api.stable.diffusion.com/v1/image/generate?text={text}")
-        image = BytesIO(response.content)
-        await ctx.send(file=discord.File(image, "image.png"))
+    async def generate_image(self, ctx, text: str):
+        api_url = "https://api.stable.zip/v1/images/generate"
+        params = {"text": text}
+        response = requests.get(api_url, params=params)
+        if response.status_code == 200:
+            image_url = response.json().get("url")
+            await ctx.send(image_url)
+        else:
+            await ctx.send("Failed to generate image.")
+
+def setup(bot):
+    bot.add_cog(ImageGeneration(bot))
