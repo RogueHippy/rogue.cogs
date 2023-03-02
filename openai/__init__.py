1ed657bc90d5ca55517533d353144247f2e079e9
RogueHippy <117488354+RogueHippy@users.noreply.github.com>
2023-03-01 21:55:35 -0700

    Update __init__.py

--- openai/__init__.py
+++ openai/__init__.py
@@ -2,8 +2,8 @@ from redbot.core.bot import Red
 
 from .openai import chatGPT
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+#with open(Path(__file__).parent / "info.json") as fp:
+#    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 async def setup(bot: Red) -> None:
