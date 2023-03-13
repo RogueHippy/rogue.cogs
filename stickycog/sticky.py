fc47e261ea0a7e89065dc59fc6e1add69a5f27cb
RogueHippy <117488354+RogueHippy@users.noreply.github.com>
2023-03-12 18:52:09 -0600

    Delay set to 20 minutes

--- stickycog/sticky.py
+++ stickycog/sticky.py
@@ -18,7 +18,7 @@ log = logging.getLogger("red.sticky")
 class Sticky(commands.Cog):
     """Sticky messages to your channels."""
 
-    REPOST_COOLDOWN = 600
+    REPOST_COOLDOWN = 1200
 
     def __init__(self, bot):
         super().__init__()
